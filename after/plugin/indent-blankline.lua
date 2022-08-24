333b5837832ca545c8900949c0d4b7fa187d5c9f
Angshuman Ghosh <ghosh.angshuman1@gmail.com>
2022-08-24 21:44:40 +0530

    Updated the indent-blankline

--- after/plugin/indent-blankline.lua
+++ after/plugin/indent-blankline.lua
@@ -1,11 +1,13 @@
+vim.opt.list = true
+vim.opt.listchars:append 'space:⋅'
+
 local status_ok, indent_blankline = pcall(require, 'indent_blankline')
 if not status_ok then
     return
 end
 
 indent_blankline.setup {
-    char = '▏',
-    show_trailing_blankline_indent = true,
+    show_trailing_blankline_indent = false,
     show_first_indent_level = true,
     use_treesitter = true,
     show_current_context = true,
