e80c34503c8fdb5d0bc25fb506bb18241694a827
angshuman <ghosh.angshuman1@gmail.com>
2023-04-09 21:20:45 +0530

    aucommands to remember folds

--- lua/angshuman/autogroups.lua
+++ lua/angshuman/autogroups.lua
@@ -2,6 +2,10 @@ local autogroups = {
     highlight_yank = {
         'TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=300}'
     },
+    remember_folds = {
+        'BufWinLeave *.* mkview',
+        'BufWinEnter *.* silent! loadview'
+    }
     --[[ set_cursor_shape = { ]]
     --[[     'VimEnter,VimResume * set guicursor=n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50' .. ]]
     --[[         ',a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor' .. ]]
