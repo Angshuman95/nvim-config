1dbc383055cc9040dc8c06a51c00f7f525f5670f
angshuman <ghosh.angshuman1@gmail.com>
2023-04-09 22:48:49 +0530

    Git in nvim tree

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -60,9 +60,14 @@ nvim_tree.setup {
         mappings = {
             list = {
                 { key = { 'l', '<CR>', 'o' }, cb = tree_cb 'edit' },
-                { key = 'h', cb = tree_cb 'close_node' },
-                { key = 'v', cb = tree_cb 'vsplit' },
+                { key = 'h',                  cb = tree_cb 'close_node' },
+                { key = 'v',                  cb = tree_cb 'vsplit' },
             },
         },
+    },
+    git = {
+        enable = true,
+        ignore = true,
+        timeout = 400,
     },
 }
