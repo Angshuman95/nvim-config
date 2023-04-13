b1b85cc4e127994dea868a54e0e0674db19085a6
angshuman <ghosh.angshuman1@gmail.com>
2023-04-13 11:53:49 +0530

    Semantic highlighting off for onedark

--- lua/angshuman/lsp/handlers.lua
+++ lua/angshuman/lsp/handlers.lua
@@ -91,6 +91,8 @@ end
 
 local tbl_doc_no_format = { 'clangd', 'html', 'tsserver' }
 
+local onedark_ok, _ = pcall(require, 'onedark')
+
 M.on_attach = function(client, bufnr)
     if contains(tbl_doc_no_format, client.name) then
         client.server_capabilities.documentFormattingProvider = false
@@ -98,7 +100,9 @@ M.on_attach = function(client, bufnr)
     lsp_keymaps(bufnr)
     lsp_highlight_document(client)
     --[[ Don't use semantic highlighting introduced in nvim 0.9 if colorscheme doesn't support ]]
-    --[[ client.server_capabilities.semanticTokensProvider = nil ]]
+    if onedark_ok then
+        client.server_capabilities.semanticTokensProvider = nil
+    end
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
