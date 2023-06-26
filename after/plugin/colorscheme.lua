febba5d91c4f8f7d995622ea0f7c7bc3f7213788
Angshuman Ghosh <ghosh.angshuman1@gmail.com>
2023-06-26 22:01:55 +0530

    Bold for types

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -7,9 +7,9 @@ if tokyonight_ok and colorscheme == 'tokyonight' then
     tokyonight.setup({
         -- your configuration comes here
         -- or leave it empty to use the default settings
-        style = 'night',        -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-        light_style = 'day',    -- The theme is used when the background is set to light
-        transparent = true,     -- Enable this to disable setting the background color
+        style = 'night', -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        light_style = 'day', -- The theme is used when the background is set to light
+        transparent = true, -- Enable this to disable setting the background color
         terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
         styles = {
             -- Style to be applied to different syntax groups
@@ -18,15 +18,16 @@ if tokyonight_ok and colorscheme == 'tokyonight' then
             keywords = { italic = true },
             functions = {},
             variables = {},
+            types = { bold = true },
             -- Background styles. Can be "dark", "transparent" or "normal"
-            sidebars = 'dark',            -- style for sidebars, see below
-            floats = 'dark',              -- style for floating windows
+            sidebars = 'dark', -- style for sidebars, see below
+            floats = 'dark', -- style for floating windows
         },
-        sidebars = { 'qf', 'help' },      -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-        day_brightness = 0.3,             -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+        sidebars = { 'qf', 'help' }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+        day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
         hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-        dim_inactive = false,             -- dims inactive windows
-        lualine_bold = true,              -- When `true`, section headers in the lualine theme will be bold
+        dim_inactive = false, -- dims inactive windows
+        lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
         --- You can override specific color groups to use other groups or a hex color
         --- function will be called with a ColorScheme table
         ---@param colors ColorScheme
@@ -49,10 +50,10 @@ local onedark_ok, onedark = pcall(require, 'onedark')
 if onedark_ok and colorscheme == 'onedark' then
     onedark.setup({
         -- Main options --
-        style = 'warmer',             -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-        transparent = true,           -- Show/hide background
-        term_colors = true,           -- Change terminal color as per the selected theme style
-        ending_tildes = true,         -- Show the end-of-buffer tildes. By default they are hidden
+        style = 'warmer', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+        transparent = true, -- Show/hide background
+        term_colors = true, -- Change terminal color as per the selected theme style
+        ending_tildes = true, -- Show the end-of-buffer tildes. By default they are hidden
         cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
 
         -- toggle theme style ---
@@ -76,6 +77,7 @@ if onedark_ok and colorscheme == 'onedark' then
             functions = 'none',
             strings = 'none',
             variables = 'none',
+            types = 'bold'
         },
 
         -- Lualine options --
@@ -84,13 +86,13 @@ if onedark_ok and colorscheme == 'onedark' then
         },
 
         -- Custom Highlights --
-        colors = {},     -- Override default colors
+        colors = {}, -- Override default colors
         highlights = {}, -- Override highlight groups
 
         -- Plugins Config --
         diagnostics = {
-            darker = true,     -- darker colors for diagnostic
-            undercurl = true,  -- use undercurl instead of underline for diagnostics
+            darker = true, -- darker colors for diagnostic
+            undercurl = true, -- use undercurl instead of underline for diagnostics
             background = true, -- use background color for virtual text
         },
     })
@@ -119,8 +121,8 @@ if catppuccin_ok and colorscheme == 'catppuccin' then
             shade = 'dark',
             percentage = 0.15,
         },
-        no_italic = false,    -- Force no italic
-        no_bold = false,      -- Force no bold
+        no_italic = false, -- Force no italic
+        no_bold = false, -- Force no bold
         no_underline = false, -- Force no underline
         styles = {
             comments = { 'italic' },
@@ -133,7 +135,7 @@ if catppuccin_ok and colorscheme == 'catppuccin' then
             numbers = {},
             booleans = {},
             properties = {},
-            types = {},
+            types = { 'bold' },
             operators = {},
         },
         color_overrides = {},
