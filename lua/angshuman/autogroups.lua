3fdbabc0824d729eee10db988d8264d26038deb5
Angshuman Ghosh <ghosh.angshuman1@gmail.com>
2023-07-19 10:51:54 +0530

    Modified to use nvim lua autocmd functions

--- lua/angshuman/autogroups.lua
+++ lua/angshuman/autogroups.lua
@@ -1,27 +1,21 @@
-local autogroups = {
-    highlight_yank = {
-        'TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=300}',
-    },
-    remember_folds = {
-        'BufWinLeave *.* mkview',
-        'BufWinEnter *.* silent! loadview',
-    },
-    --[[ set_cursor_shape = { ]]
-    --[[     'VimEnter,VimResume * set guicursor=n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50' .. ]]
-    --[[         ',a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor' .. ]]
-    --[[         ',sm:block-blinkwait175-blinkoff150-blinkon175' ]]
-    --[[ }, ]]
-    --[[ restore_cursor_shape = { ]]
-    --[[     'VimLeave,VimSuspend * set guicursor=a:ver90-blinkwait300-blinkon200-blinkoff150' ]]
-    --[[ } ]]
-}
+local api = vim.api
 
-for group_name, definition in pairs(autogroups) do
-    vim.api.nvim_command('augroup ' .. group_name)
-    vim.api.nvim_command('autocmd!')
-    for _, def in ipairs(definition) do
-        local command = table.concat(vim.tbl_flatten({ 'autocmd', def }), ' ')
-        vim.api.nvim_command(command)
-    end
-    vim.api.nvim_command('augroup END')
-end
+-- Highlight on yank
+local yank_grp = api.nvim_create_augroup('yank_highlight', { clear = true })
+api.nvim_create_autocmd('TextYankPost', {
+    command = "silent! lua vim.highlight.on_yank{higroup='IncSearch', timeout=300}",
+    group = yank_grp,
+})
+
+-- Remember Folds
+local fold_grp = api.nvim_create_augroup('remember_folds', { clear = true })
+api.nvim_create_autocmd('BufWinLeave', {
+    pattern = '*.*',
+    command = 'mkview',
+    group = fold_grp,
+})
+api.nvim_create_autocmd('BufWinEnter', {
+    pattern = '*.*',
+    command = 'silent! loadview',
+    group = fold_grp,
+})
