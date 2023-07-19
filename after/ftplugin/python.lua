95d156be1f59bb2c408ccc570d8a4569803aed9d
Angshuman Ghosh <ghosh.angshuman1@gmail.com>
2023-07-19 21:43:44 +0530

    Added Code runner logics in AutoGroups

--- after/ftplugin/python.lua
+++
@@ -1,1 +0,0 @@
-vim.api.nvim_set_keymap('n', '<leader>c', ':!clear; python3 %<CR>', {})
--- lua/angshuman/autogroups.lua
+++ lua/angshuman/autogroups.lua
@@ -19,3 +19,28 @@ api.nvim_create_autocmd('BufWinEnter', {
     command = 'silent! loadview',
     group = fold_grp,
 })
+
+-- Code Build and Run
+api.nvim_create_autocmd('BufEnter', {
+    pattern = '*',
+    callback = function()
+        vim.schedule(CodeRunner)
+    end,
+})
+
+function CodeRunner()
+    local ft = vim.bo.filetype
+
+    if ft == 'python' then
+        print('Hello')
+        vim.api.nvim_set_keymap('n', '<leader>c', ':!clear; python3 %<CR>', {})
+    elseif ft == 'cpp' then
+        print('Hello')
+        vim.api.nvim_set_keymap(
+            'n',
+            '<leader>c',
+            ':!clear; clang++ -g -Wall -Wextra %;  if [ -f a.out ]; then time ./a.out; rm a.out; fi <CR>',
+            {}
+        )
+    end
+end
