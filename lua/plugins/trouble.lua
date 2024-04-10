c3862bb9f8b65882ada956e3fbe9bc739edf77b1
Angshuman Ghosh <ghosh.angshuman1@gmail.com>
2024-04-10 11:14:18 +0530

    Using trouble v3

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,48 +1,56 @@
 return {
     'folke/trouble.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-        require('trouble').setup({
-            mode = 'document_diagnostics',
-        })
-        vim.keymap.set(
-            'n',
-            '<leader>ww',
-            '<cmd>TroubleToggle workspace_diagnostics<cr>',
-            { silent = true, noremap = true }
-        )
-        vim.keymap.set(
-            'n',
-            '<leader>wd',
-            '<cmd>TroubleToggle document_diagnostics<cr>',
-            { silent = true, noremap = true }
-        )
-        vim.keymap.set(
-            'n',
-            '<leader>wl',
-            '<cmd>TroubleToggle loclist<cr>',
-            { silent = true, noremap = true }
-        )
-        vim.keymap.set(
-            'n',
-            '<leader>wq',
-            '<cmd>TroubleToggle quickfix<cr>',
-            { silent = true, noremap = true }
-        )
-        vim.keymap.set(
-            'n',
-            'gR',
-            '<cmd>TroubleToggle lsp_references<cr>',
-            { silent = true, noremap = true }
-        )
-    end,
-    cmd = 'TroubleToggle',
-    keys = {
-        {
-            '<leader>w',
-            '<cmd>TroubleToggle<cr>',
-            silent = true,
-            noremap = true,
+    branch = 'dev',
+    opts = {
+        modes = {
+            preview_float = {
+                mode = 'diagnostics',
+                preview = {
+                    type = 'float',
+                    relative = 'editor',
+                    border = 'rounded',
+                    title = 'Preview',
+                    title_pos = 'center',
+                    position = { 0, -2 },
+                    size = { width = 0.3, height = 0.3 },
+                    zindex = 200,
+                },
+            },
         },
     },
+
+    keys = {
+        {
+            '<leader>xx',
+            '<cmd>Trouble diagnostics toggle<cr>',
+            desc = 'Diagnostics (Trouble)',
+        },
+        {
+            '<leader>xX',
+            '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+            desc = 'Buffer Diagnostics (Trouble)',
+        },
+        {
+            '<leader>cs',
+            '<cmd>Trouble symbols toggle focus=false<cr>',
+            desc = 'Symbols (Trouble)',
+        },
+        {
+            '<leader>cl',
+            '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+            desc = 'LSP Definitions / references / ... (Trouble)',
+        },
+        {
+            '<leader>xl',
+            '<cmd>Trouble loclist toggle<cr>',
+            desc = 'Location List (Trouble)',
+        },
+        {
+            '<leader>xq',
+            '<cmd>Trouble qflist toggle<cr>',
+            desc = 'Quickfix List (Trouble)',
+        },
+    },
+    cmd = 'Trouble',
 }
