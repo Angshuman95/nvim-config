29aea52185abee66b518258e6e96a2bc7bb9688e
Angshuman Ghosh <ghosh.angshuman1@gmail.com>
2024-08-03 13:11:42 +0530

    Added markdown.nvim and corrected better-escape

--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -2,6 +2,7 @@ return {
     'max397574/better-escape.nvim',
     config = function()
         require('better_escape').setup({
+            default_mappings = false,
             mappings = {
                 i = {
                     j = {
