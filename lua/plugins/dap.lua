6e0151dc804ad62dcf86f70d097bba2aa32ce065
Angshuman Ghosh <ghosh.angshuman1@gmail.com>
2025-05-02 00:43:59 +0530

    Dap corrected

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -10,6 +10,26 @@ return {
             { 'theHamsta/nvim-dap-virtual-text', config = true },
         },
         cmd = { 'DapUIToggle', 'DapToggleRepl', 'DapToggleBreakpoint' },
+        keys = {
+            {
+                '<leader>db',
+                '<cmd>lua require("dap").toggle_breakpoint()<cr>',
+                noremap = true,
+                silent = true,
+            },
+            {
+                '<leader>du',
+                '<cmd>lua require("dapui").toggle()<cr>',
+                noremap = true,
+                silent = true,
+            },
+            {
+                '<leader>dr',
+                '<cmd>lua require("dap").repl.toggle()<cr>',
+                silent = true,
+                noremap = true,
+            },
+        },
         config = function()
             vim.fn.sign_define('DapBreakpoint', {
                 text = '',
@@ -19,7 +39,7 @@ return {
             })
             vim.fn.sign_define(
                 'DapStopped',
-                { text = '', texthl = '', linehl = '', numhl = '' }
+                { text = '󰜴', texthl = '', linehl = '', numhl = '' }
             )
             vim.fn.sign_define(
                 'DapBreakpointRejected',
@@ -31,66 +51,74 @@ return {
 
             keymap(
                 'n',
-                '<A-b>',
-                '<cmd>lua require"dap".toggle_breakpoint()<cr>',
-                opts
-            )
-            keymap('n', '<A-h>', '<cmd>lua require"dap".continue()<cr>', opts)
-            keymap('n', '<A-l>', '<cmd>lua require"dap".step_into()<cr>', opts)
-            keymap('n', '<A-j>', '<cmd>lua require"dap".step_over()<cr>', opts)
-            keymap('n', '<A-k>', '<cmd>lua require"dap".step_out()<cr>', opts)
-            keymap(
-                'n',
-                '<leader>dr',
-                '<cmd>lua require"dap".repl.toggle()<cr>',
-                opts
-            )
-            keymap(
-                'n',
-                '<leader>dl',
-                '<cmd>lua require"dap".run_last()<cr>',
-                opts
-            )
-            keymap(
-                'n',
-                '<leader>dt',
-                '<cmd>lua require"dap".terminate()<cr>',
-                opts
-            )
-            keymap(
-                'n',
-                '<leader>du',
-                '<cmd>lua require"dapui".toggle()<cr>',
-                opts
-            )
-            keymap(
-                'n',
-                '<leader>dn',
-                '<cmd>lua require"dap".run_to_cursor()<cr>',
-                opts
-            )
-            keymap(
-                'n',
-                '<leader>dR',
-                '<cmd>lua require"dap".clear_breakpoints()<cr>',
-                opts
-            )
-            keymap(
-                'n',
-                '<leader>da',
-                '<cmd>lua require"debugHelper".attach()<cr>',
-                opts
-            )
-            keymap(
-                'n',
-                '<leader>dA',
-                '<cmd>lua require"debugHelper".attachToRemote()<cr>',
+                '<leader>dc',
+                '<cmd>lua require("dap").continue()<cr>',
                 opts
             )
             keymap(
                 'n',
                 '<leader>di',
-                '<cmd>lua require"dap.ui.widgets".hover()<cr>',
+                '<cmd>lua require("dap").step_into()<cr>',
+                opts
+            )
+            keymap(
+                'n',
+                '<leader>do',
+                '<cmd>lua require("dap").step_over()<cr>',
+                opts
+            )
+            keymap(
+                'n',
+                '<leader>dO',
+                '<cmd>lua require("dap").step_out()<cr>',
+                opts
+            )
+            keymap(
+                'n',
+                '<leader>dl',
+                '<cmd>lua require("dap").run_last()<cr>',
+                opts
+            )
+            keymap(
+                'n',
+                '<leader>dt',
+                '<cmd>lua require("dap").terminate()<cr>',
+                opts
+            )
+            keymap(
+                'n',
+                '<leader>dC',
+                '<cmd>lua require("dap").run_to_cursor()<cr>',
+                opts
+            )
+            keymap(
+                'n',
+                '<leader>dq',
+                '<cmd>lua require("dapui").open({ reset = true })<cr>',
+                opts
+            )
+            keymap(
+                'n',
+                '<leader>dR',
+                '<cmd>lua require("dap").clear_breakpoints()<cr>',
+                opts
+            )
+            keymap(
+                'n',
+                '<leader>da',
+                '<cmd>lua require("debugHelper").attach()<cr>',
+                opts
+            )
+            keymap(
+                'n',
+                '<leader>dA',
+                '<cmd>lua require("debugHelper").attachToRemote()<cr>',
+                opts
+            )
+            keymap(
+                'n',
+                '<leader>dw',
+                '<cmd>lua require("dap.ui.widgets").hover()<cr>',
                 opts
             )
 
