5944a40f68936e56634bb8978ec3dce0d5ddf57c
Angshuman Ghosh <ghosh.angshuman1@gmail.com>
2025-05-15 23:42:53 +0530

    Added Snacks!!!

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,108 @@
+return {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    opts = {
+        explorer = { enabled = true },
+        picker = { enabled = true },
+        indent = { enabled = true },
+        statuscolumn = { enabled = true },
+        terminal = { enabled = true, win = { style = 'terminal' } },
+        scratch = { enabled = true },
+    },
+    keys = {
+        {
+            '<leader>e',
+            function()
+                Snacks.explorer()
+            end,
+            desc = 'File Explorer',
+            silent = true,
+        },
+        {
+            '<leader>f',
+            function()
+                Snacks.picker.files()
+            end,
+            silent = true,
+        },
+        {
+            '<leader>b',
+            function()
+                Snacks.picker.buffers()
+            end,
+            silent = true,
+        },
+        {
+            '<leader>st',
+            function()
+                Snacks.picker.grep()
+            end,
+            silent = true,
+        },
+        {
+            '<leader>sD',
+            function()
+                Snacks.picker.diagnostics()
+            end,
+            silent = true,
+            noremap = true,
+        },
+        {
+            '<leader>sd',
+            function()
+                Snacks.picker.diagnostics_buffer()
+            end,
+            silent = true,
+        },
+        {
+            '<leader>ss',
+            function()
+                Snacks.picker.lsp_symbols()
+            end,
+            silent = true,
+        },
+        {
+            '<leader>sS',
+            function()
+                Snacks.picker.lsp_workspace_symbols()
+            end,
+            silent = true,
+        },
+        {
+            '<leader>sm',
+            function()
+                Snacks.picker.marks()
+            end,
+            silent = true,
+        },
+        {
+            '<leader>sj',
+            function()
+                Snacks.picker.jumps()
+            end,
+            silent = true,
+        },
+        {
+            '<leader>sq',
+            function()
+                Snacks.picker.qflist()
+            end,
+            silent = true,
+        },
+        {
+            '<leader>.',
+            function()
+                Snacks.scratch()
+            end,
+            silent = true,
+        },
+        {
+            '<c-\\>',
+            function()
+                Snacks.terminal()
+            end,
+            silent = true,
+        },
+    },
+}
--- lua/plugins/statuscol.lua
+++
@@ -1,7 +0,0 @@
-return {
-    'luukvbaal/statuscol.nvim',
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-        require('statuscol').setup()
-    end,
-}
