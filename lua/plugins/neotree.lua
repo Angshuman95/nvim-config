3b0af45592b3644b71454cbfec708d3c623aca14
Angshuman Ghosh <ghosh.angshuman1@gmail.com>
2025-06-07 22:32:31 +0530

    Added options for neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -18,5 +18,28 @@ return {
         filesystem = {
             follow_current_file = { enabled = true },
         },
+        window = {
+            mappings = {
+                ['l'] = 'open',
+                ['h'] = 'close_node',
+                ['Y'] = {
+                    function(state)
+                        local node = state.tree:get_node()
+                        local path = node:get_id()
+                        vim.fn.setreg('+', path, 'c')
+                    end,
+                    desc = 'Copy Path to Clipboard',
+                },
+                ['O'] = {
+                    function(state)
+                        require('lazy.util').open(
+                            state.tree:get_node().path,
+                            { system = true }
+                        )
+                    end,
+                    desc = 'Open with System Application',
+                },
+            },
+        },
     },
 }
