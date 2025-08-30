686bb6c280e423b6aa28f5f6d3d74b136a41a7d2
Angshuman Ghosh <ghosh.angshuman1@gmail.com>
2025-08-30 11:23:31 +0530

    Autosave only on command

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -2,7 +2,6 @@ return {
     'okuuva/auto-save.nvim',
     version = '^1.0.0',
     cmd = 'ASToggle',
-    event = { 'InsertLeave', 'TextChanged' },
     opts = {
         debounce_delay = 1800,
     },
