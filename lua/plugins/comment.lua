744dcb8c4a26e79c5422cb0aa1ff8896b2a85785
Angshuman Ghosh <ghosh.angshuman1@gmail.com>
2025-08-30 13:36:09 +0530

    nvim ts commentstring on filetype

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -58,6 +58,6 @@ return {
             require('ts_context_commentstring').setup()
             vim.g.skip_ts_context_commentstring_module = true
         end,
-        event = { 'BufReadPre', 'BufNewFile' },
+        ft = { 'ts', 'js' },
     },
 }
