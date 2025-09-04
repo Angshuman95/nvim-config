97d2a2687d10b74e35c2c4c3192369c127114a20
angshumang65 <Angshuman.Ghosh@hyland.com>
2025-09-04 20:35:35 +0530

    Added window picker for neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,43 +1,55 @@
 return {
-    'nvim-neo-tree/neo-tree.nvim',
-    branch = 'v3.x',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        'nvim-tree/nvim-web-devicons',
-        'MunifTanjim/nui.nvim',
+    {
+        's1n7ax/nvim-window-picker',
+        name = 'window-picker',
+        event = 'VeryLazy',
+        version = '2.*',
+        config = function()
+            require('window-picker').setup()
+        end,
     },
-    cmd = { 'Neotree' },
-    keys = {
-        {
-            '<leader>e',
-            ':Neotree<CR>',
-            silent = true,
+    {
+        'nvim-neo-tree/neo-tree.nvim',
+        branch = 'v3.x',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-tree/nvim-web-devicons',
+            'MunifTanjim/nui.nvim',
+            's1n7ax/nvim-window-picker',
         },
-    },
-    opts = {
-        filesystem = {
-            follow_current_file = { enabled = true },
+        cmd = { 'Neotree' },
+        keys = {
+            {
+                '<leader>e',
+                ':Neotree<CR>',
+                silent = true,
+            },
         },
-        window = {
-            mappings = {
-                ['l'] = 'open',
-                ['h'] = 'close_node',
-                ['Y'] = {
-                    function(state)
-                        local node = state.tree:get_node()
-                        local path = node:get_id()
-                        vim.fn.setreg('+', path, 'c')
-                    end,
-                    desc = 'Copy Path to Clipboard',
-                },
-                ['O'] = {
-                    function(state)
-                        require('lazy.util').open(
-                            state.tree:get_node().path,
-                            { system = true }
-                        )
-                    end,
-                    desc = 'Open with System Application',
+        opts = {
+            filesystem = {
+                follow_current_file = { enabled = true },
+            },
+            window = {
+                mappings = {
+                    ['l'] = 'open',
+                    ['h'] = 'close_node',
+                    ['Y'] = {
+                        function(state)
+                            local node = state.tree:get_node()
+                            local path = node:get_id()
+                            vim.fn.setreg('+', path, 'c')
+                        end,
+                        desc = 'Copy Path to Clipboard',
+                    },
+                    ['O'] = {
+                        function(state)
+                            require('lazy.util').open(
+                                state.tree:get_node().path,
+                                { system = true }
+                            )
+                        end,
+                        desc = 'Open with System Application',
+                    },
                 },
             },
         },
