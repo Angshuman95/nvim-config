9f6a18d6eaaaadad11c249880c03837ba8ef1245
Angshuman Ghosh <ghosh.angshuman1@gmail.com>
2025-10-21 20:14:41 +0530

    Added code companion features

--- lua/plugins/code-companion.lua
+++ lua/plugins/code-companion.lua
@@ -6,7 +6,23 @@ return {
         'CodeCompanionActions',
         'CodeCompanionCmd',
     },
-    opts = {},
+    opts = {
+        strategies = {
+            chat = {
+                adapter = {
+                    name = 'copilot',
+                    model = 'claude-sonnet-4',
+                },
+            },
+        },
+        memory = {
+            opts = {
+                chat = {
+                    enabled = true,
+                },
+            },
+        },
+    },
     dependencies = {
         'nvim-lua/plenary.nvim',
         'nvim-treesitter/nvim-treesitter',
