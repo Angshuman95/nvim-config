428691ea0d68c39094b176533899c12c25d7d9e6
angshumang65 <Angshuman.Ghosh@hyland.com>
2025-10-24 14:24:53 +0530

    Updated leap config

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -3,8 +3,26 @@ return {
         'ggandor/leap.nvim',
         dependencies = 'tpope/vim-repeat',
         config = function()
-            require('leap').add_default_mappings()
+            vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
+            vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
+
+            local leap = require('leap')
+
+            leap.opts.preview = function(ch0, ch1, ch2)
+                return not (
+                    ch1:match('%s')
+                    or (ch0:match('%a') and ch1:match('%a') and ch2:match('%a'))
+                )
+            end
+
+            leap.opts.equivalence_classes = {
+                ' \t\r\n',
+                '([{',
+                ')]}',
+                '\'"`',
+            }
+
+            require('leap.user').set_repeat_keys('<enter>', '<backspace>')
         end,
-        event = { 'BufReadPre', 'BufNewFile' }
-    }
+    },
 }
