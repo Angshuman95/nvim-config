bc9ba7ebd8c6c09fec0d251f6ff0876666dfe52b
Angshuman Ghosh <ghosh.angshuman1@gmail.com>
2025-12-10 13:15:51 +0530

    removed nvim-comment

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -5,28 +5,4 @@ return {
             require('Comment').setup()
         end,
     },
-    {
-        'terrortylor/nvim-comment',
-        config = function()
-            require('nvim_comment').setup({
-                create_mappings = false,
-            })
-        end,
-        keys = {
-            {
-                '<leader>//',
-                '<cmd>CommentToggle<CR>',
-                'n',
-                silent = true,
-                noremap = true,
-            },
-            {
-                '<leader>/',
-                '<cmd>CommentToggle<CR>',
-                mode = 'v',
-                silent = true,
-                noremap = true,
-            },
-        },
-    }
 }
