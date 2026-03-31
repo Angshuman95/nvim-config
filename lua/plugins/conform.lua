7350df60c1c0369fbf4cdf6de7a0d3a43632fa20
angshumang65 <Angshuman.Ghosh@hyland.com>
2026-03-31 20:35:23 +0530

    added csharper options in conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -71,6 +71,11 @@ return {
                 black = {
                     args = { '--fast', '--quiet', '-' },
                 },
+                csharpier = {
+                    command = 'dotnet-csharpier',
+                    args = { '--write-stdout' },
+                    stdin = true,
+                },
             },
 
             format_on_save = function(_)
