16f8faa0f4ab053a1e5a40e7d46984cc48a8774d
angshumang65 <Angshuman.Ghosh@hyland.com>
2026-04-03 11:55:05 +0530

    Updated the default model of codecompanion

--- lua/plugins/code-companion.lua
+++ lua/plugins/code-companion.lua
@@ -11,7 +11,7 @@ return {
             chat = {
                 adapter = {
                     name = 'copilot',
-                    model = 'claude-sonnet-4.5',
+                    model = 'claude-sonnet-4.6',
                 },
             },
         },
