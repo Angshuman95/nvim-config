1a1108744726e02a78814aadf41b5665c1ae3f49
angshuman <ghosh.angshuman1@gmail.com>
2026-06-22 22:06:21 +0530

    Now using deepseek for home config

--- lua/plugins/code-companion.lua
+++ lua/plugins/code-companion.lua
@@ -10,27 +10,44 @@ return {
         'CodeCompanionActions',
         'CodeCompanionCmd',
     },
-    opts = {
-        strategies = {
-            chat = {
-                adapter = {
-                    name = 'copilot',
-                    model = 'claude-sonnet-4.6',
+    opts = function()
+        local adapter_config
+        if os.getenv('DEEPSEEK_API_KEY') then
+            adapter_config = 'deepseek'
+        else
+            adapter_config = {
+                name = 'copilot',
+                model = 'claude-sonnet-4.6',
+            }
+        end
+        return {
+            adapters = {
+                deepseek = function()
+                    return require('codecompanion.adapters').extend(
+                        'deepseek',
+                        {
+                            env = {
+                                api_key = os.getenv('DEEPSEEK_API_KEY'),
+                            },
+                        }
+                    )
+                end,
+            },
+            strategies = {
+                chat = {
+                    adapter = adapter_config,
+                },
+                inline = {
+                    adapter = adapter_config,
                 },
             },
-            inline = {
-                adapter = {
-                    name = 'copilot',
-                    model = 'claude-sonnet-4.6',
+            extensions = {
+                history = {
+                    opts = {
+                        picker = 'snacks',
+                    },
                 },
             },
-        },
-        extensions = {
-            history = {
-                opts = {
-                    picker = 'snacks',
-                },
-            },
-        },
-    },
+        }
+    end,
 }
