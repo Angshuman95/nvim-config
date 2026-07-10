ca33bdd5698264c725ee9bc67c6583404950da2b
angshuman <ghosh.angshuman1@gmail.com>
2026-07-10 21:54:49 +0530

    Obsidian vsplit

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -4,7 +4,6 @@ return {
         local cwd = vim.fn.getcwd()
         local workspace_paths = {
             'personal-notes',
-            '10_00-Office-Notes',
         }
 
         for _, path in ipairs(workspace_paths) do
@@ -17,46 +16,53 @@ return {
     dependencies = {
         'nvim-lua/plenary.nvim',
     },
-    opts = {
-        workspaces = {
-            {
-                name = 'mac-notes',
-                path = '~/Developer/notes_and_files/10-19-Learning/10-Notes/10_00-Personal-Notes/personal-notes',
+    config = function()
+        require('obsidian').setup({
+            workspaces = {
+                {
+                    name = 'arch-notes',
+                    path = '~/Developer/10-19-learning/10-notes/personal-notes',
+                },
             },
-            {
-                name = 'office-notes',
-                path = '~/Developer/notes_and_files/10-19-Learning/10-Notes/10_01-Office-Notes/Notes-And-Files/10-19-Learning/10-Notes/10_00-Office-Notes',
+            notes_subdir = 'notes',
+            new_notes_location = 'current_dir',
+            note_id_func = function(title)
+                if title ~= nil then
+                    return title
+                        :gsub(' ', '-')
+                        :gsub('[^A-Za-z0-9-]', '')
+                        :lower()
+                end
+                return tostring(os.time())
+            end,
+            daily_notes = {
+                folder = 'journals',
+                date_format = '%Y/%m-%B/%Y-%m-%d-daily-note',
+                alias_format = nil,
+                default_tags = { 'daily-notes' },
+                template = 'daily-note.md',
             },
-            {
-                name = 'arch-notes',
-                path = '~/Developer/10-19-learning/10-notes/personal-notes',
+            templates = {
+                folder = 'templates',
+                date_format = '%Y-%m-%d',
+                time_format = '%H:%M:%S',
+                substitutions = {},
             },
-        },
-        notes_subdir = 'notes',
-        new_notes_location = 'current_dir',
-        note_id_func = function(title)
-            if title ~= nil then
-                return title:gsub(' ', '-'):gsub('[^A-Za-z0-9-]', ''):lower()
-            end
-            return tostring(os.time())
-        end,
-        daily_notes = {
-            folder = 'journals',
-            date_format = '%Y/%m-%B/%Y-%m-%d-daily-note',
-            alias_format = nil,
-            default_tags = { 'daily-notes' },
-            template = 'daily-note.md',
-        },
-        templates = {
-            folder = 'templates',
-            date_format = '%Y-%m-%d',
-            time_format = '%H:%M:%S',
-            substitutions = {},
-        },
-        ui = { enable = false }, -- using markdown.nvim
-        picker = {
-            name = 'snacks.pick',
-        },
-        legacy_commands = false,
-    },
+            ui = { enable = false }, -- using markdown.nvim
+            picker = {
+                name = 'snacks.picker',
+            },
+            legacy_commands = false,
+
+            vim.keymap.set(
+                'n',
+                '<leader>v',
+                '<cmd>Obsidian follow_link vsplit_force<CR>',
+                {
+                    desc = 'Open Obsidian link in vsplit',
+                    silent = true,
+                }
+            ),
+        })
+    end,
 }
