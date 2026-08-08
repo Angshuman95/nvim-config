8ef065c850a549d5550501a916e3d433ee8738d8
angshuman <ghosh.angshuman1@gmail.com>
2026-08-08 23:31:30 +0530

    Remove unused colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,55 +1,14 @@
 return {
-    {
-        'catppuccin/nvim',
-        name = 'catppuccin',
-        priority = 1000,
-        opts = {
-            transparent_background = true,
-            styles = {
-                comments = { 'italic' },
-                conditionals = { 'italic' },
-                types = { 'bold' },
-            },
-            auto_integrations = true,
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    opts = {
+        transparent_background = true,
+        styles = {
+            comments = { 'italic' },
+            conditionals = { 'italic' },
+            types = { 'bold' },
         },
-    },
-    {
-        'folke/tokyonight.nvim',
-        lazy = true,
-        opts = {
-            style = 'night',
-            transparent = true,
-            terminal_colors = true,
-            styles = {
-                comments = { italic = true },
-                conditionals = { italic = true },
-                types = { bold = true },
-            },
-        },
-    },
-    {
-        'navarasu/onedark.nvim',
-        lazy = true,
-        opts = {
-            style = 'warmer',
-            transparent = true,
-            code_style = {
-                comments = 'italic',
-                conditionals = 'italic',
-                types = 'bold',
-            },
-
-            -- Lualine options --
-            lualine = {
-                transparent = true, -- lualine center bar transparency
-            },
-
-            -- Plugins Config --
-            diagnostics = {
-                darker = true,     -- darker colors for diagnostic
-                undercurl = true,  -- use undercurl instead of underline for diagnostics
-                background = true, -- use background color for virtual text
-            },
-        },
+        auto_integrations = true,
     },
 }
